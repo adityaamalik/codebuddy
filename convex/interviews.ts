3afc0b9d989fd9a537f76fad03b1ebb00968d8bd
Varilla <44826959+adityaamalik@users.noreply.github.com>
2025-06-08 15:03:43 +0200

    Meeting room components
    Code editor, problem statement section and meeting controls section complete

--- convex/interviews.ts
+++ convex/interviews.ts
@@ -1,76 +1,72 @@
 import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 
-// get all interviews
-export const getInterviews = query({
-    handler: async (ctx) => {
-        const identity = await ctx.auth.getUserIdentity();
-        if (!identity) throw new Error("User is not authenticated");
+export const getAllInterviews = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("Unauthorized");
 
-        const interviews = await ctx.db.query("interviews").collect();
-        return interviews;
-    }
+    const interviews = await ctx.db.query("interviews").collect();
+
+    return interviews;
+  },
 });
 
-// get an interview by id
 export const getMyInterviews = query({
-    handler: async (ctx) => {
-        const identity = await ctx.auth.getUserIdentity();
-        if (!identity) return [];
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) return [];
 
-        const interviews = 
-            await ctx.db
-            .query("interviews")
-            .withIndex("by_candidate_id", (q) => q.eq("candidateId", identity.subject))
-            .collect();
-            
-        return interviews!;
-    }
+    const user = await ctx.db.query("users")
+      .withIndex("byClerkId", (q) => q.eq("clerkId", identity.subject))
+      .first();
+    
+    if (!user) return [];
+
+    return await ctx.db
+      .query("interviews")
+      .withIndex("by_candidate_id", (q) => q.eq("candidateId", user._id))
+      .collect();
+  },
 });
 
-// get interview by stream call id
 export const getInterviewByStreamCallId = query({
-    args: {
-        streamCallId: v.string(),
-    },
-    handler: async (ctx, args) => {
-        return await ctx.db.query("interviews").filter((q) => q.eq(q.field("streamCallId"), args.streamCallId)).first();
-    }
+  args: { streamCallId: v.string() },
+  handler: async (ctx, args) => {
+    return await ctx.db
+      .query("interviews")
+      .withIndex("by_stream_call_id", (q) => q.eq("streamCallId", args.streamCallId))
+      .first();
+  },
 });
 
-// create an interview
 export const createInterview = mutation({
-    args: {
-        title: v.string(),
-        description: v.optional(v.string()),
-        startTime: v.number(),
-        status: v.string(),
-        streamCallId: v.string(),
-        candidateId: v.string(),
-        interviewerIds: v.array(v.string()),
-    },
-    handler: async (ctx, args) => {
-        const identity = await ctx.auth.getUserIdentity();
-        if (!identity) throw new Error("User is not authenticated");
+  args: {
+    title: v.string(),
+    description: v.string(),
+    startTime: v.number(),
+    status: v.string(),
+    streamCallId: v.string(),
+    candidateId: v.id("users"),
+    interviewerId: v.array(v.string()),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("Unauthorized");
 
-        return await ctx.db.insert("interviews", {
-            ...args,
-        });
-    },
+    return await ctx.db.insert("interviews", args);
+  },
 });
 
 export const updateInterviewStatus = mutation({
-    args: {
-        id: v.id("interviews"),
-        status: v.string(),
-    },
-    handler: async (ctx, args) => {
-        const identity = await ctx.auth.getUserIdentity();
-        if (!identity) throw new Error("User is not authenticated");
-
-        return await ctx.db.patch(args.id, {
-            status: args.status,
-            ...(args.status == "completed" ? { endTime: Date.now() } : {}),
-        });
-    },
+  args: {
+    id: v.id("interviews"),
+    status: v.string(),
+  },
+  handler: async (ctx, args) => {
+    return await ctx.db.patch(args.id, {
+      status: args.status,
+      ...(args.status === "completed" ? { endTime: Date.now() } : {}),
+    });
+  },
 });
